bfa10221639856404938fcc1902782ed7e276190
Swati Amlani <swatiamlani@gmail.com>
2020-02-10 00:37:14 +0530

    kl

--- merge.js
+++ merge.js
@@ -28,19 +28,26 @@ function main(
 
   const bigquery = new BigQuery();
 
+// Import the Google Cloud client libraries
 
+  const {Storage} = require('@google-cloud/storage');
+
+  const storage = new Storage();
+
+  const bucketName = 'bucket1-swati';
+
+  const filename = 'emp.json';
+   
+   
+   
   
-  async function createDataset() {
-
-    
+  async function createDataset() {   
 
     const options = {
 
       location: 'US',
 
     };
-
-
 
     // Create a new dataset
 
@@ -49,6 +56,10 @@ function main(
     console.log(`Dataset ${dataset.id} created.`);
 
   }
+  
+  
+  
+  
 
   async function createTable() {
 
@@ -60,7 +71,6 @@ function main(
 
     // const schema = 'Name:string, Age:integer, Weight:float, IsMagic:boolean';
 
-
     const options = {
 
       schema: schema,
@@ -69,24 +79,88 @@ function main(
 
     };
 
-
-
-    // Create a new table in the dataset
-
+   // Create a new table in the dataset
     const [table] = await bigquery
 
       .dataset(datasetId)
 
       .createTable(tableId, options);
 
-
-
     console.log(`Table ${table.id} created.`);
-
   } 
 
+  
+  async function loadJSONFromGCS() {
+
+    // Imports a GCS file into a table with manually defined schema.
+
+
+    var datasetId = "my_new_dataset_SA";
+     const tableId = "table1_swati";
+
+
+    const metadata = {
+
+      sourceFormat: 'NEWLINE_DELIMITED_JSON',
+
+      schema: {
+
+        fields: [
+
+            {name: 'userId', type: 'STRING'},
+
+            {name: 'firstName', type: 'STRING'},
+        
+            {name: 'lastName', type: 'STRING'},
+        
+            {name: 'phoneNumber', type: 'STRING'},
+        
+            {name: 'emailAddress', type: 'STRING'},
+
+        ],
+
+      },
+
+      location: 'US',
+
+    };
+
+
+
+    // Load data from a Google Cloud Storage file into the table
+
+    const [job] = await bigquery
+
+      .dataset(datasetId)
+
+      .table(tableId)
+
+      .load(storage.bucket(bucketName).file(filename), metadata);
+
+    // load() waits for the job to finish
+
+    console.log(`Job ${job.id} completed.`);
+
+
+
+    // Check the job's status for errors
+
+    const errors = job.status.errors;
+
+    if (errors && errors.length > 0) {
+
+      throw errors;
+
+    }
+
+  }
+  
+  
+  
+  
   createDataset();
   createTable();
+  loadJSONFromGCS();
 
 }
 
