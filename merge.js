723980e9e048516eb94481d7b49761f5f8892ec8
Swati Amlani <swatiamlani@gmail.com>
2020-02-10 00:22:19 +0530

    fg

--- merge.js
+++ merge.js
@@ -3,7 +3,7 @@
 
 function main(
 
-  datasetId = 'my_table', // Existing dataset
+  datasetId = 'my_new_dataset_SA', // Existing dataset
 
   tableId = 'table1_swati', // Table to be created
 
@@ -34,7 +34,7 @@ function main(
 
     // Creates a new table named "my_table" in "my_dataset".
 
-     const datasetId = "my_new_dataset_SA";
+     var datasetId = "my_new_dataset_SA";
 
     // const tableId = "my_table";
 
